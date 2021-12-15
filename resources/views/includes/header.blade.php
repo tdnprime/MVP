8801d53a76459557f23dfd0c5c77786fcc1a19ba
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2021-12-15 22:35:15 +0000

    @include meta,header,menu and footer for all layouts

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <a href="#" title="Menu" id='menu-icon'>
+        <span class="material-icons">menu</span></a>
+        <a id='logo' href="/home" title='Boxeon home'>
+            <img id='logo' src='../assets/images/logo.svg' alt='logo'/>
+            <span id="beta">Beta</span></a>
+
+    @auth
+
+    <a id='current-user' href='/box/{{ $user ? $user->id : '0'}}/edit' title='Edit mode'>
+        <span class='material-icons'>account_box</span> {{$user->name}}'s Boxeon
+    </a>
+
+    @else
+    <a id='signin' href='{{ url('auth/google') }}'>
+        Sign in with Google
+    </a>
+
+    @endauth
+</header>
---
+++ resources/views/includes/meta.blade.php
@@ -0,0 +1,18 @@
+<title>Best way for creators to get sustainable income and connect with fans | Boxeon</title>
+<meta name="description" content="Start and grow a subscription box business and secure monthly income without making extra content. It's the wave of the future."/>
+<meta name="keywords" content="Boxeon, Patreon alternative, monitize content, Subscription box platform, make more money on Youtube">
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<link rel="icon" type="image/svg+xml" href="assets/images/favicon.svg">
+<link rel="alternate icon" href="assets/images/favicon.svg">
+<link rel="mask-icon" href="https://boxeon.com/images/favicon.svg" color="#fff">
+<link rel="stylesheet" href="assets/css/style.css?v=2">
+<link rel="stylesheet" media="screen and (min-width: 200px) and (max-width: 1591px)" href="assets/css/mobile.css?v=1.0"/>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open%20Sans">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Concert%20One">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="assets/js/global.js?v=1"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-211880503-1">
+</script>

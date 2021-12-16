59789ff672d62e1988bed152c37b0262ec083bae
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2021-12-16 21:54:12 +0000

    current changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,17 +1,13 @@
 
 {{-- WARNING #3 Of 3
-    
+
     MAKE URL ABSOLUTE FOR PRODUCTION--}}
 
 <header>
     <a href="#" title="Menu" id='menu-icon'>
         <span class="material-icons">menu</span></a>
         <a id='logo' href="/home" title='Boxeon home'>
-<<<<<<< Updated upstream
             <img id='logo' src='{{ asset('../assets/images/logo.svg') }}' alt='logo'/>
-=======
-            <img id='logo' src='http://localhost:8000/assets/images/logo.svg' alt='logo'/>
->>>>>>> Stashed changes
             <span id="beta">Beta</span></a>
 
     @auth

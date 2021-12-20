fa19457f78278b84aae4eab73d835da1c27a553c
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2021-12-20 16:21:25 -0500

    UI updated

--- resources/views/subscription_box/edit.blade.php
+++ resources/views/subscription_box/edit.blade.php
@@ -4,15 +4,15 @@
 <div id="masthead" class="fadein">
     
     <div id="headline">
-        <div class="two-col-grid">
+     
             <div><p class="text-heading-label">FANS ARE WAITING</p><h1 class="ginormous">Create a subscription box today</h1>
     <p id="pitch">If you started creating a box and chose to have us help you with product curation and shipping, we will be
     contacting you by email. Please ensure our emails aren't in your spam folder.</p>
     <a class="button" href="/box/create">Create box</a>
 </div>
-   
+   </div>
     <div id="masthead-image-construction"></div>
-</div>
-</div>
+
+
 </div>
 @endsection

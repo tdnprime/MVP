4e027d2361c6bb655c178c37eb58409e244873e4
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2021-12-29 10:06:20 -0500

    Custom url edits

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -25,20 +25,15 @@ class ShippingController extends Controller
     public function rates(User $user, Request  $request)
     {
 
-
         if (json_decode($_SERVER[ "HTTP_TO" ])  !== null) {
             $to = json_decode($_SERVER[ "HTTP_TO" ]);
            dd($to);
         }else{
             echo "Missing header";
         }
-
         $id = $to->id;
         $user = User::find($id);
         $box = $user->boxes()->first();
-
-       // dd($box);
-
 
         if (json_decode($_SERVER[ "HTTP_TO" ])  !== null) {
             $to = json_decode($_SERVER[ "HTTP_TO" ]);

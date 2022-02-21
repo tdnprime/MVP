817b16bf70407c005cbaacbc6b410518c0ef7d45
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-02-21 12:05:59 -0500

    Square ready for live test

--- laravel/resources/views/vendor/mail/html/footer.blade.php
+++ laravel/resources/views/vendor/mail/html/footer.blade.php
@@ -3,7 +3,8 @@
 <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
 <td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
+{{ Illuminate\Mail\Markdown::parse($slot) }}        
+<img src="https://www.google-analytics.com/collect?v=1&..."/>
 </td>
 </tr>
 </table>

35a4fbfd2e5baf55a498c7c11ba6eb41b440c522
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-03-02 13:25:58 -0500

    UI updated

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -13,7 +13,7 @@
     </div>
 
 
-    <main> <a id='whatis' href='#whatis'></a> 
+    <main id='margin-top-45-em'> <a id='whatis' href='#whatis'></a> 
     <aside class="asides">
       <h1 class="extra-large-font darkblue">What's a subscription box?</h1>
       <p class="centered center font-1-5-em">In this business model, content creators put together a box of products around a theme, and their most loyal fans subscribe to receive these boxes for the thematic experience. </p>

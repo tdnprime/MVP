8bf96fce8c228cb6fa7dd30c3c1119fc0e9f7cf7
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-03-09 15:20:01 -0500

    rolled back 4

--- laravel/app/Jobs/SendEmailJob.php
+++ laravel/app/Jobs/SendEmailJob.php
@@ -7,7 +7,6 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\Middleware\ThrottlesExceptions;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;

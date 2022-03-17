563856500fc655579013c00576b7451d48ad7cea
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-03-17 19:28:04 -0400

    updated

--- laravel/app/Jobs/ScrapeYoutube.php
+++ laravel/app/Jobs/ScrapeYoutube.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use Alaouy\Youtube\Facades\Youtube;
-use App\Http\Controllers\YoutubeController;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -37,8 +36,69 @@ class ScrapeYoutube implements ShouldQueue
     {
         dispatch(function () {
 
-            YoutubeController::search($this->tag);
-            
+           
+        // Same params as before
+        $params = [
+            'q' => $keyword,
+            'type' => 'channel',
+            'part' => 'id, snippet',
+            'maxResults' => 50,
+        ];
+
+        $pageTokens = [];
+
+        // Make inital search
+        $search = Youtube::paginateResults($params, null);
+        // Store token
+        $pageTokens[] = $search['info']['nextPageToken'];
+        // Go to next page in result
+        $search = Youtube::paginateResults($params, $pageTokens[0]);
+        // Store token
+        $pageTokens[] = $search['info']['nextPageToken'];
+        // Go to next page in result
+        $search = Youtube::paginateResults($params, $pageTokens[1]);
+        // Store token
+        $pageTokens[] = $search['info']['nextPageToken'];
+        // Go back a page
+        // $search = Youtube::paginateResults($params, $pageTokens[0]);
+
+        // Add results key with info parameter set
+        foreach ($search['results'] as $obj) {
+
+            try {
+
+                $channel = Youtube::getChannelById($obj->snippet->channelId);
+
+                if ($channel->statistics->videoCount > 0) {
+
+                    $average_views = $channel->statistics->viewCount / $channel->statistics->videoCount;
+
+                } else {
+                    $average_views = 0;
+                }
+
+                if ($average_views > 10000) {
+
+
+                        DB::table('_creators_')->insertOrIgnore([
+
+                            'channel_id' => $obj->snippet->channelId,
+                            'channel_name' => $obj->snippet->title,
+                            'country' => $channel->snippet->country ?? null,
+                            'views' => $channel->statistics->viewCount,
+                            'videos' => $channel->statistics->videoCount,
+
+                        ]);
+                  
+                }
+
+            } catch (exception $e) {
+
+                continue;
+            }
+
+        }
+
         })->afterResponse();
 
     }

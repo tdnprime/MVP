599be3ff13ee0ff0a69223ec1a4b4b6b0fb903ff
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-03-24 09:57:32 -0400

    extension ready

---
+++ laravel/app/Http/Controllers/ExtensionController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class ExtensionController extends Controller
+{
+    public function fetch(){
+
+        $creators = DB::table('_creators_')
+        ->where('email', '=', null)
+        ->orderBy('id', 'desc')
+        ->limit(49)
+        ->get();
+
+        return json_encode($creators);
+    }
+
+    public function save(Request $request){
+
+        $channel = json_decode($request['channel']);
+
+        DB::table('_creators_')
+        ->where('id', '=', $channel->id)
+        ->update(['email' => $channel->email]);
+
+        return;
+
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -124,8 +124,9 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
         Route::post('/set', 'App\Http\Controllers\YoutubeController@set');
         Route::get('/update/key', 'App\Http\Controllers\YoutubeController@deleteCookie');
         Route::get('/populate', 'App\Http\Controllers\YoutubeController@populate');
-        Route::get('/search', 'App\Jobs\YoutubeSearch@search');
-
+        Route::get('/fetch', 'App\Http\Controllers\ExtensionController@fetch');
+        Route::get('/extsave', 'App\Http\Controllers\ExtensionController@save');
+        Route::post('/extsave', 'App\Http\Controllers\ExtensionController@save');
 
 
 

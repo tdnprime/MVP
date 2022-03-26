f109cfb9e0a580a3ba025cd8188b85f610ae90aa
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-03-26 05:35:17 -0400

    ready

--- laravel/php/scrape.php
+++ laravel/php/scrape.php
@@ -1,23 +1,44 @@
 <?php
 
-use Laravel\App\Jobs\ScrapeYoutubeJob;
-use Laravel\Illuminate\Support\Facades\DB;
+use App\Jobs\ScrapeYoutubeJob;
+use Illuminate\Support\Facades\DB;
 
 
 
-$tags = DB::table('tags')
-    ->where('status', '=', 0)
-    ->orderBy('id', 'desc')
-    ->limit(49)
-    ->get();
 
-foreach ($tags as $keyword) {
+function populate()
+{
 
-    DB::table('tags')
-        ->where('id', '=', $keyword->id)
-        ->update(['status' => 0]);
+    $keys = [
 
-    ScrapeYoutubeJob::dispatch($keyword->tag)->onQueue('scrape')
-        ->delay(now()->addMinutes(1));
+      'AIzaSyCkvTo6KCIPZN2tcCr_mSbpK94HWbvZpAo',
+      'AIzaSyC3cOLS4KvLW0FfnOtVxRvf9qGDroNpZuc',
+      'AIzaSyBneHI51930L1b_yJYJZ0Iy-d0BPsfKBFw',
+      'AIzaSyCg1sR5FdvwU91cxJT-dj-nJVodg7DRhf4',
+      'AIzaSyCtfj-I5p6EJ2_VmGEvX6_QyQw4PHoSZew'
+    ];
+
+    foreach ($keys as $key) {
+
+        $tags = DB::table('tags')
+            ->where('status', '=', 0)
+            ->orderBy('id', 'desc')
+            ->limit(1)
+            ->get();
+
+        foreach ($tags as $keyword) {
+            
+          $count += 1;
+          ScrapeYoutubeJob::dispatch($keyword->tag, $key)->onQueue('scrape')
+          ->delay(now()->addMinutes(1));
+
+            DB::table('tags')
+            ->where('id', '=', $keyword->id)
+            ->update(['status' => 1]);
+
+        }
+       
+    }
 
 }
+populate();

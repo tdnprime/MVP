427cae7886f8661d7b0dc8be409d63ec8deb796e
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-04-12 10:33:58 -0400

    testing

--- laravel/app/Http/Controllers/SquareController.php
+++ laravel/app/Http/Controllers/SquareController.php
@@ -320,7 +320,7 @@ class SquareController extends Controller
                 "name" => "Boxeon",
             ]]);
 
-        $response = json_decode($response);
+        $response = json_decode($response); dd($response);
 
         if (isset($response->subscription->id)) {
 

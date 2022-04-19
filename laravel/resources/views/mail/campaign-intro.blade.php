e526d3d66209eb087af08e0fb20e0334831884b2
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-04-19 14:29:34 -0400

    ready 5

--- laravel/resources/views/mail/campaign-intro.blade.php
+++ laravel/resources/views/mail/campaign-intro.blade.php
@@ -1,39 +1,41 @@
 @component('mail::message')
-    # Hello {{ ucfirst($creator->channel_name) }},
-    <br>
-    <p>Boxeon would like to partner with you.</p>
 
-    <p>Boxeon is a platform for creators to offer subscription boxes to their fans. Boxeon is like Patreon but for
-        subscription boxes.</p>
+# Hello {{ucfirst( $creator->channel_name )}},
 
-    <p>You will receive 86% of the profits.</p>
+<br>
+Boxeon would like to partner with you. 
 
-    <p>If necessary, we will help with warehousing, packaging, and fulfillment.</p>
+<br>
 
-    <p>Starting capital won't be an issue. We may offer the subscription box in pre-sales for the first four weeks and use
-        pre-sales funds to acquire and ship initial inventory.</p>
+Boxeon is a platform for creators to offer subscription boxes to their fans. Boxeon is like Patreon but for subscription boxes.
 
-    <p>We suggest that the first set of boxes are offered in support of Ukraine relief efforts. We do have a product curator
-        who is Ukrainian and she will be able to help curate a box of Ukrainian-made products.</p>
+You will receive 86% of the profits.
 
-    <p>For more info, please check out https://boxeon.com/partner</p>
+If necessary, we will help with warehousing, packaging, and fulfillment.
 
-    <br><br>
+Starting capital won't be an issue. We may offer the subscription box in pre-sales for the first four weeks and use pre-sales funds to acquire and ship initial inventory.
 
-    Sincerely,<br>
-    Trevor Prime, CEO<br>
-    https://boxeon.com<br>
-    E: trevor@boxeon.com<br>
-    WhatsApp: +1 718 674 2890‬
-    LinkedIn: https://www.linkedin.com/company/boxeon
+We suggest that the first set of boxes are offered in support of Ukraine relief efforts. We do have a product curator
+who is Ukrainian and she will be able to help curate a box of Ukrainian-made products.
 
-    <br><br>
+For more info, please check out https://boxeon.com/partner
 
-    CONFIDENTIALITY NOTICE: This message is from Boxeon LLC and may contain confidential
-    business information. It is intended solely for the use of the individual to
-    whom it is addressed. If you are not the intended recipient please contact the
-    sender and delete this message and any attachment from your system.
-    Unauthorized publication, use, dissemination, forwarding, printing or
-    copying of this E-Mail and its attachments is strictly prohibited.
-    <img src="https://www.google-analytics.com/collect?v=1&tid=G-EKYP1LECWS&t=event&ec=email&ea=open" />
+<br><br>
+
+Sincerely,<br>
+Trevor Prime, CEO<br>
+https://boxeon.com<br>
+E: trevor@boxeon.com<br>
+WhatsApp: +1 718 674 2890‬
+LinkedIn: https://www.linkedin.com/company/boxeon
+
+<br><br>
+
+CONFIDENTIALITY NOTICE: This message is from Boxeon LLC and may contain confidential 
+business information. It is intended solely for the use of the individual to 
+whom it is addressed. If you are not the intended recipient please contact the 
+sender and delete this message and any attachment from your system. 
+Unauthorized publication, use, dissemination, forwarding, printing or 
+copying of this E-Mail and its attachments is strictly prohibited.
+<img src="https://www.google-analytics.com/collect?v=1&tid=G-EKYP1LECWS&t=event&ec=email&ea=open"/>
 @endcomponent

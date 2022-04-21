1b1b15a00987900ca51c5bbd406f520a0395379b
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-04-21 16:17:24 -0400

    unsub

--- laravel/resources/views/mail/unsubscribe.blade.php
+++ laravel/resources/views/mail/unsubscribe.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Boxeon | Unsubscribed')
 @section('content')
 <section class='section'>
-<img id='image-error-page' class='center display-block margin-top-4-em' src='../assets/images/page_not_found.svg' alt='Maintenance workers'/>
+<img id='image-error-page' class='center display-block margin-top-4-em' src='../assets/images/unsub.svg' alt='Unsubscribed'/>
 <h1 class='centered primary-color'>You've been unsubscribed from our mailing list.</h1>
 </section>
 </div>

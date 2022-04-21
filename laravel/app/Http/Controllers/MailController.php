e55b0838da1198b8ae95808672b7823bd8c86ab4
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-04-21 12:25:27 -0400

    TESTING

--- laravel/app/Http/Controllers/MailController.php
+++ laravel/app/Http/Controllers/MailController.php
@@ -12,33 +12,7 @@ use Illuminate\Support\Facades\DB;
 class MailController extends Controller
 {
 
-    public function test(){
-
-       
-            $id = auth()->user()->id;
-            $sent = 0;
-            $creators = DB::table('mailing_list')
-            ->where('campaign', '<>', '4')
-            ->where('country', '=', 'US')
-            ->where('valid', '=', '1')
-            ->orderBy('channel_name', 'desc')
-            ->limit(1)
-            ->select('*')
-            ->get();
-    
-            $num = count($creators);
-            for ($i = 0; $i < $num; $i++) {
-    
-                $creator = (object) $creators[$i];
-                #Queue an order-placed system email
-                $details['email'] = 'trevorprimenyc@gmail.com'; 
-                return $message = new Campaign($creator);
-                SendEmailJob::dispatch($details, $message)->onQueue('emails')
-                ->delay(now()->addMinutes(1));
-    
-            }
-
-    }
+   
 
     public function send()
     {

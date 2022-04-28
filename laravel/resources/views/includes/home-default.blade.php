aab42228c83617d723ed5d7e2e912293d6822072
Trevor Prime <89152062+tdnprime@users.noreply.github.com>
2022-04-28 12:09:43 -0400

    cta updated

--- laravel/resources/views/includes/home-default.blade.php
+++ laravel/resources/views/includes/home-default.blade.php
@@ -1,7 +1,7 @@
 <div class='centered margin-bottom-4-em'>
     <img class='center image-cta' src="{{ '../assets/images/no-box.svg' }}" alt="Empty">
-    <h2 class='centered'>Your home</h2>
-    <p class='center'>Keep track of subscriptions and subscribers when you have them.</p>
+    <h2 class='centered'>Start here</h2>
+    <p class='center'>Apply for a remote job or find your favorite creators.</p>
     <br><br>
     <a class='button' href='/partner'>Apply now</a>     <a class='button clearbtn' href='/search/creator'>Find creators</a>
 </div>
